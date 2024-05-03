1195466cfc89228e09ea5b64df4f1aee845b350f
Kamal Nayan <geniusamansingh@gmail.com>
2024-05-03 12:15:28 +0530

    components added and connect wallet btn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css'
+import '../vars.css'
+import type {AppProps} from 'next/app'
+import {ChakraProvider} from "@chakra-ui/react";
+import theme from '../styles/Theme';
+import Navbar from "../components/Navbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WalletProvider } from "@suiet/wallet-kit";
+import "@suiet/wallet-kit/style.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+
+const queryClient = new QueryClient({
+defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
+
+  return (
+      <ChakraProvider theme={theme}>
+            <WalletProvider>
+        <QueryClientProvider client={queryClient}>
+          <div className={"customBackground"}>
+            <Navbar/>
+            <Component {...pageProps} />
+          </div>
+        </QueryClientProvider>
+        </WalletProvider>>
+      </ChakraProvider>
+  )
 }
+
+export default MyApp
